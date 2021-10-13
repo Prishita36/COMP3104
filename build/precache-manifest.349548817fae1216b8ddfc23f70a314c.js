194ea232efec21b3ae57eb349e2d21be8b89cdef
Prishita36 <prishita36@gmail.com>
2021-10-13 19:57:34 -0400

    adding build folder

---
+++ build/precache-manifest.349548817fae1216b8ddfc23f70a314c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d84c84442666707c17b4",
+    "url": "/Prishita36/COMP3104/static/css/main.86343568.chunk.css"
+  },
+  {
+    "revision": "d84c84442666707c17b4",
+    "url": "/Prishita36/COMP3104/static/js/main.d84c8444.chunk.js"
+  },
+  {
+    "revision": "ce5f6bd8f5c5f437ec6f",
+    "url": "/Prishita36/COMP3104/static/js/runtime~main.ce5f6bd8.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Prishita36/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "1463c4bc3f2fcfde453858734a886fac",
+    "url": "/Prishita36/COMP3104/index.html"
+  }
+];
